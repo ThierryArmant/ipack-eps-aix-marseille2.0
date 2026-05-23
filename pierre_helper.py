1c5b8786b324da9e9e979320ecbc95f34b1a008e
ThierryArmant <thierryarmant@gmail.com>
2026-05-23 09:12:07 +0200

    Update pierre_helper.py

--- pierre_helper.py
+++ pierre_helper.py
@@ -2,7 +2,7 @@ import os
 from llama_index.core import SimpleDirectoryReader
 
 def get_pierre_docs():
-    if os.path.exists("Géré par pierre.txt"):
+    if os.path.exists("gere_par_pierre.txt"):
         try:
             return SimpleDirectoryReader(input_files=["gere_par_pierre.txt"]).load_data()
         except:

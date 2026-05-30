427fc1cc95eca995cba1acf93f2c96a676599213
ThierryArmant <thierryarmant@gmail.com>
2026-05-30 14:57:50 +0200

    tout semble fonctionner la prochaine mofid sera ajour de couleur sur les titres et liens

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from llama_index.core.memory import ChatMemoryBuffer
 st.set_page_config(
     page_title="Hub IA - EPS", 
     layout="wide", 
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="collapsed" 
 )
 
 # ======================================================================

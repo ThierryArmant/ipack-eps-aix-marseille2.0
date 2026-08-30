366fb1e8cfb2d574e7189965f8c071428f8d663d
ThierryArmant <thierryarmant@gmail.com>
2026-08-30 10:48:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -553,7 +553,7 @@ else:
 # 8. ZONE D'ACTION
 # ======================================================================
 prompt = st.chat_input(
-    "Posez votre question institutionnelle, technique ou juridique ici...",
+    "Posez votre question institutionnelle, technique ou juridique ici... Attention, pour éviter les erreurs d'interprétation cliquez sur le module concerné : IpackEPS ou Examens ou sécurité",
     key="chat_main",
 )
 
